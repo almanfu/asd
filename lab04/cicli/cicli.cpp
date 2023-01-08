cdbe22ab856e5b8a0ab9818ffa5db839d8b68100
Alessandro Manfucci <alessandro.manfucci@gmail.com>
2023-01-08 07:45:23 +0100

    cicli 70/100

--- lab04/cicli/cicli.cpp
+++ lab04/cicli/cicli.cpp
@@ -18,14 +18,20 @@ class Graph{
   enum GraphType {DIRECTED=false, UNDIRECTED=true};
 
   vector<list<Node>> adj;
-  vector<vector<int>> shortest;
+  Node root;
+  vector<Node> parent;
+  vector<Node> level;
   int n;
   GraphType _graphType;
+  bool _hasCycle;
 
   Graph(int nodes, GraphType gType){
     _graphType = gType;
     n = nodes;
     adj = vector<list<Node>>(n);
+    parent = vector<Node>(n);
+    level = vector<Node>(n);
+    _hasCycle = false;
   }
   ~Graph(){}
   bool has(Node u){
@@ -36,6 +42,9 @@ class Graph{
   }
   const bool undirected(){
     return _graphType == UNDIRECTED;
+  }
+  const bool hasCycle(){
+    return _hasCycle;
   }
   void insertEdge(Node u, Node v){
     if(has(u) && has(v) && u != v){
@@ -67,19 +76,27 @@ class Graph{
     vector<bool> visited = vector<bool>(n, false);
     if(has(r)){
       q.push(r);
+      root = r;
       visited[r] = true;
+      parent[r] = -1;
+      level[r] = 0;
     }
     while(!q.empty()){
       Node u = q.front(); q.pop();
-      cout << u << ' ';
+      //cout << u << ' ';
       for(Node v: adj[u]){
+        if(visited[v] && v != parent[u])
+          _hasCycle = true;
+
         if(!visited[v]){
           q.push(v);
           visited[v] = true;
+          parent[v] = u;
+          level[v] = level[u]+1;
         }
       }
     }
-    cout << endl;
+    //cout << endl;
   }
 
   void dfs(Node r){
@@ -102,11 +119,9 @@ class Graph{
     cout << endl;
   }
 
-  vector<int> distance(Node r){
-    queue<Node> q;
-    vector<int> _distance(n);
-    for(Node u=0; u < n; u++)
-      _distance[u] = -1;
+  int distance(Node r, Node d){
+    queue<Node> q = queue<Node>();
+    vector<int> _distance(n, -1);
     if(has(r)){
       q.push(r);
       _distance[r] = 0;
@@ -117,49 +132,46 @@ class Graph{
         if(_distance[v] == -1){
           _distance[v] = 1+_distance[u];
           q.push(v);
-        }
-      }
-    }
-    return _distance;  
-  }
-
-  vector<int> ccs(){
-    if(!undirected())
-      return vector<int>();
-
-    vector<int> cc = vector<int>(n, -1);
-    int i = 0;
-    for(Node r=0; r < n; r++){
-      if(cc[r] == -1){
-        //BFS
-        queue<Node> q;
-        q.push(r);
-        cc[r] = i;
-        while(!q.empty()){
-          Node u = q.front(); q.pop();
-          for(Node v: adj[u]){
-            if(cc[v] == -1){
-              q.push(v);
-              cc[v] = i;
-            }
+          if(v == d){
+            q = queue<Node>();
+            break;
           }
         }
-        i++;
       }
     }
-    return cc;
+    return _distance[d];  
   }
 
-  void setup(){// calculate all pairs shortest-path
-    shortest = vector<vector<int>>(n);
-    for(Node r=0; r < n; r++)
-      shortest[r] = distance(r);
+  int distance_tree(Node s, Node d){
+    queue<Node> q = queue<Node>();
+    int i=0;
+    Node sanc = s;
+    Node danc = d;
+    while(level[s]-i > level[d]){
+      sanc = parent[sanc];
+      i++;
+    }
+    i=0;
+    while(level[d]-i > level[s]){
+      danc = parent[danc];
+      i++;
+    }
+
+    while(sanc != danc){
+      sanc = parent[sanc];
+      danc = parent[danc];
+    }
+
+    return level[s]+level[d]-2*level[sanc];      
   }
 
-  int cicli(Node u, Node v){
-    return shortest[u][v];
+  int cicli(Node s, Node d){
+    if(hasCycle()){
+      return distance(s, d);
+    }else{
+      return distance_tree(s, d);
+    }
   }
-
 };
 
 /*
@@ -183,12 +195,15 @@ int main(int argc, char *argv[]){
     g.insertEdge(u, v);
   }
 
-  g.setup();
+  // setup
+  g.bfs((n-1)/2);
+
+  //cout << g.hasCycle() ;
 
   for(int i=0; i<q; i++){
-    Graph::Node u, v;
-    in >> u >> v;
-    out << g.cicli(u, v) << endl;
+    Graph::Node r, d;
+    in >> r >> d;
+    out << g.cicli(r, d) << endl;
   }
 
   in.close();
